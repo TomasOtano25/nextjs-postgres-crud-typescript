057a480f19ff5ff4c170ba3adf0d48295b91948a
Tomas Garcia <tomasotano25@gmail.com>
2022-10-25 20:34:59 -0400

    crear tareas

---
+++ database/db.sql
@@ -0,0 +1,11 @@
+
+-- \c <database>
+
+CREATE TABLE IF NOT EXISTS tasks(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(200),
+  created_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO tasks(title, description) VALUES ('Task 1', 'Description 1');
--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,17 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Methods } from "../../../utils/enums/methods.enum";
+import { conn } from "../../../utils/database";
+
+type Task = {
+  title: string;
+  description: string;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.method, req.url);
 
-  const { method } = req;
+  const { method, body } = req;
 
   switch (method) {
     case Methods.GET:
       return res.status(200).json("getting tasks");
     case Methods.POST:
-      return res.status(201).json("creating task");
+      const { title, description } = body as Task;
+
+      const query = `INSERT INTO tasks(title, description) VALUES ($1, $2) RETURNING *`;
+      const values = [title, description];
+
+      const response = await conn.query(query, values);
+
+      return res.status(201).json(response.rows[0]);
     default:
       return res.status(400).json("invalid method");
   }
