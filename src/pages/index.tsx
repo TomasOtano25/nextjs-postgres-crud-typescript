51170dc3eb2a45e267aa438df6c57a8afc161422
Tomas Garcia <tomasotano25@gmail.com>
2022-10-26 00:04:09 -0400

    add chakra ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function index() {
-  return <div>index</div>;
+import { Task } from "src/types/Task";
+
+type Props = {
+  tasks: Task[];
+};
+
+export default function Index({ tasks }: Props) {
+  if (tasks.length === 0) return <h1>No tasks</h1>;
+
+  return <h1></h1>;
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch("http://localhost:3001/api/tasks");
+  const tasks: Task[] = await res.json();
+
+  return {
+    props: {
+      tasks,
+    },
+  };
+};
